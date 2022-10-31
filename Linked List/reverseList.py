84de36b9db0a75f30a2cd759a6fe4d8c274e6fc4
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-10-31 17:20:33 +0530

    Create reverseList.py

---
+++ Linked List/reverseList.py
@@ -0,0 +1,36 @@
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        
+        return newHead
+        
+        
+        
+        # Recursive: T O(n), M O(n)
+        if head == None or head.next == None:
+            return head
+        
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        
+        return p
+        
+        
+        
+        
+        
+        # Iterative: T O(n), M O(n)
+        prev, curr = None, head
+        while curr:
+            nextP = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextP
+        return prev
